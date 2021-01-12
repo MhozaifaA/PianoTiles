2e9906ce53278fdede233af38d878cdfef9df9ee
MhozaifaA <botchey.mha@hotmail.com>
2021-01-12 22:56:45 +0200

    project with server

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component'; 
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PianoTilesComponent } from './pianoTiles/pianoTiles.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    PianoTilesComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'pianoTiles', component: PianoTilesComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]  
+})
+export class AppModule { }
---
+++ ClientApp/src/app/pianoTiles/pianoTiles.component.ts
@@ -0,0 +1,10 @@
+import { Component   } from '@angular/core';
+
+@Component({
+  selector: 'app-pianoTiles',
+  templateUrl: './pianoTiles.component.html',
+})
+
+export class PianoTilesComponent {
+ 
+}
