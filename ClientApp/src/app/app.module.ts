fca3d3ff7997e779cfd2ec2591618973ad52c952
MhozaifaA <botchey.mha@hotmail.com>
2022-01-09 18:23:25 +0200

    Refactoring

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component'; 
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { PianoTilesComponent } from './pianoTiles/pianoTiles.component';
 import { TileComponent } from './pianoTiles/tile.component';
 import { FilterColumn } from './pipes/filterColumn.pipe';
@@ -18,8 +16,6 @@ import { FilterColumn } from './pipes/filterColumn.pipe';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     PianoTilesComponent,
     FilterColumn,
     TileComponent
@@ -30,8 +26,6 @@ import { FilterColumn } from './pipes/filterColumn.pipe';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'pianoTiles', component: PianoTilesComponent },
     ])
   ],
